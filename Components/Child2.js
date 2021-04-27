4f197c988c592b76c2b7874ab120fe697d95c88a
ddrdusk <83031647+ddrdusk@users.noreply.github.com>
2021-04-26 21:29:19 -0300

    Update Child2.js

--- Components/Child2.js
+++ Components/Child2.js
@@ -2,7 +2,7 @@ import { Component } from "react";
 
 class Child extends Component{
   
-  frutasEnStock = () => this.props.frutas.filter(fruta => fruta.cantidad > 0).map(fruta => <li>{fruta.nombre}</li>) /*pone un span o algo*/
+  frutasEnStock = () => this.props.frutas.filter(fruta => fruta.cantidad > 0).map(fruta => <li>{fruta.nombre}<span onClick{this.props.onEliminar}>Eliminar!</span></li>) /*pone un span o algo*/
 
   frutasSinStock = () => this.props.frutas.filter(fruta => fruta.cantidad === 0).map(fruta => <li>{fruta.nombre}</li>)
 
