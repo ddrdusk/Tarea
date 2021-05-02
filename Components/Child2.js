5c80118e11422db986371d34bd1923ac2a8bbda3
ddrdusk <83031647+ddrdusk@users.noreply.github.com>
2021-05-02 17:32:52 -0300

    Update Child2.js

--- Components/Child2.js
+++ Components/Child2.js
@@ -1,34 +1,33 @@
+import './styles.css';
+import ProductsList from "./Components/ProductsList";
 import React from 'react';
-class ProductsList extends React.Component
-{
-    
-  handleClick = () =>
-  {
-    this.props.onEliminar("eliminate")
-    /*filtrar productos*/
-    
+class App extends React.Component{
+  state = {
+    products: [
+      { id: 1, name: "monitor", count: 12 },
+      { id: 2, name: "impresora", count: 1 },
+      { id: 3, name: "mouse", count: 33 },
+      { id: 4, name: "alcohol", count: 0 },
+      { id: 5, name: "cap", count: 0 },
+    ]
   }
-  render()
-  {
-    return (
-    <ol>
-    {this.props.products.map( product => {
+  /*delete = itemId => {
+    const filtredData = this.state.products.filter(item => item.id !== itemId);
+    this.setState({ products: filtredData });
+  }*/
+  render() {
       return (
-      <li key={product.id}>{`nombre: ${product.name} - cantidad: ${product.count}`} <span onClick={this.handleClick}>[Eliminar!!]</span></li>
-      )
-    }
-    )}
-    </ol>
-    )
+        <div className="App">
+          <header className="App-header">
+            <h3>Todos los productos</h3>
+            <ProductsList products={this.state.products} onEliminar={(mensaje)=> alert(mensaje)} /*onClick={() => this.delete(ProductsList,1)}*/ />
+            {/* <h4>Productos sin stock</h4>
+            <ProductsList products={this.state.products.filter( (product) => product.count <= 0 )} />
+            <h4>Productos con stock</h4>
+            <ProductsList products={this.state.products.filter( (product) => product.count > 0 )} /> */}
+          </header>
+        </div>
+  );
   }
-    /*
-  delete = itemId => {
-    //En vez de usar item.id, usa la propiedad
-    //que te ayuda a identificar tu objeto que quieres borrar.
-    //Quizas clavearticulo sea la adecuada.
-    const filtredData = this.state.items.filter(item => item.id !== itemId);
-    this.setState({ items: filtredData });
-  };
-  <button onClick={() => this.delete(item.id)}>Eliminar</button>*/
-}
-export default ProductsList;
+};
+export default App;
