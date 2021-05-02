472e77073431cf7bba17fabc12903e024cae49e1
ddrdusk <83031647+ddrdusk@users.noreply.github.com>
2021-05-02 17:32:12 -0300

    Update Child2.js

--- Components/Child2.js
+++ Components/Child2.js
@@ -5,6 +5,8 @@ class ProductsList extends React.Component
   handleClick = () =>
   {
     this.props.onEliminar("eliminate")
+    /*filtrar productos*/
+    
   }
   render()
   {
@@ -19,45 +21,14 @@ class ProductsList extends React.Component
     </ol>
     )
   }
-    /*import React from "react";
-
-export default class App extends React.Component {
-  state = {
-    items: [
-      { id: 1, title: "Titulo" },
-      { id: 2, title: "Titulo" },
-      { id: 3, title: "Titulo" }
-    ]
-  };
-
+    /*
   delete = itemId => {
-    //En vez de uasr item.id, usa la propiedad
+    //En vez de usar item.id, usa la propiedad
     //que te ayuda a identificar tu objeto que quieres borrar.
     //Quizas clavearticulo sea la adecuada.
     const filtredData = this.state.items.filter(item => item.id !== itemId);
     this.setState({ items: filtredData });
   };
-
-  render() {
-    return (
-      <table style={{ width: "100%", textAlign: "center" }}>
-        <tr>
-          <th># -</th>
-          <th>Titulo</th>
-          <th>Acción</th>
-        </tr>
-        {this.state.items.map((item, key) => (
-          <tr key={key}>
-            <td>{item.id}</td>
-            <td>{item.title + item.id}</td>
-            <td>
-              <button onClick={() => this.delete(item.id)}>Eliminar</button>
-            </td>
-          </tr>
-        ))}
-      </table>
-    );
-  }
-}*/
+  <button onClick={() => this.delete(item.id)}>Eliminar</button>*/
 }
 export default ProductsList;
