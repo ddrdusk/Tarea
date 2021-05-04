7b284b6eb85d7ee1ea3bf8bd8f98b4157c79c772
ddrdusk <83031647+ddrdusk@users.noreply.github.com>
2021-05-03 21:35:35 -0300

    Update App.js

--- listaTareas/App.js
+++ listaTareas/App.js
@@ -1,3 +1,6 @@
 /*
-hacer una lista de tares que desaparezca cuando apretas un boton
+hacer 3 listas de tareas
+una de terminadas
+otra de pendientes y otra para todas
+que cambie de hecha a pendiente con un boton
 */
