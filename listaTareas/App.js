9aca1b38dedd13ce07a36f3e0830d80be4fb4a95
ddrdusk <83031647+ddrdusk@users.noreply.github.com>
2021-05-07 16:53:52 -0300

    Update App.js

--- listaTareas/App.js
+++ listaTareas/App.js
@@ -5,8 +5,19 @@ otra de pendientes y otra para todas
 que cambie de hecha a pendiente con un boton
 */
 import "./styles.css";
+import React from 'react';
+//import Boton from "./Component/Button"
 
 export default function App() {
+  /*state = {
+    products: [
+      { id: 1, name: "monitor", count: 12 },
+      { id: 2, name: "impresora", count: 1 },
+      { id: 3, name: "mouse", count: 33 },
+      { id: 4, name: "alcohol", count: 0 },
+      { id: 5, name: "cap", count: 0 },
+    ]
+  }*/
   return (
   <div className="container">
     <div className="page-header">
@@ -30,4 +41,4 @@ export default function App() {
     </div>
   </div>
   );
-}
+}//</Boton>
