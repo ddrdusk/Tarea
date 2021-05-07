c47ceb7b484925fef6560b58ebcd6908ad7d90a3
ddrdusk <83031647+ddrdusk@users.noreply.github.com>
2021-05-07 16:59:44 -0300

    Update Button.js

--- listaTareas/Button.js
+++ listaTareas/Button.js
@@ -3,22 +3,23 @@
 import React from 'react';
 class Boton extends React.Component
 {
-    handleClick = (productId) =>
-    {
-        this.props.onEliminar(productId)
-    }
-    render() 
-    {
-        return ( 
-                <ol>
-        {this.props.products.map( product => {
+  handleClick = (productId) =>
+  {
+    this.props.onEliminar(productId)
+  }
+  render()
+  {
+    return (
+      <ul>
+        {this.props.products.map( product => 
+          {
             return (
-                <li key={product.id}>{`nombre: ${product.name} - cantidad: ${product.count}`} <span onClick={() => this.handleClick(product.id)}>[Eliminar!!]</span></li>
+              <li key={product.id}>{`nombre: ${product.name} - cantidad: ${product.count}`} <span onClick={() => this.handleClick(product.id)}>[Eliminar!!]</span></li>
             )
-        }
+          }
         )}
-    </ol>
-        )
-    }
+      </ul>
+    )
+  }
 }
 export default Boton;
