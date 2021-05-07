7d9097175529bc4fa4b27ca7f1308c224f6ef088
ddrdusk <83031647+ddrdusk@users.noreply.github.com>
2021-05-07 16:54:53 -0300

    Update Button.js

--- listaTareas/Button.js
+++ listaTareas/Button.js
@@ -1,3 +1,24 @@
-/*
-  Hacer funciones para el boton y cambiar listas
-*/
+//Hacer funciones para el boton y cambiar listas
+
+import React from 'react';
+class Boton extends React.Component
+{
+    handleClick = (productId) =>
+    {
+        this.props.onEliminar(productId)
+    }
+    render() 
+    {
+        return ( 
+                <ol>
+        {this.props.products.map( product => {
+            return (
+                <li key={product.id}>{`nombre: ${product.name} - cantidad: ${product.count}`} <span onClick={() => this.handleClick(product.id)}>[Eliminar!!]</span></li>
+            )
+        }
+        )}
+    </ol>
+        )
+    }
+}
+export default Boton;
