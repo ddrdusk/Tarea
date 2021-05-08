59e96edd205f563944227c195e82deb9016b4653
ddrdusk <83031647+ddrdusk@users.noreply.github.com>
2021-05-08 15:39:25 -0300

    Update Button.js

--- listaTareas/Button.js
+++ listaTareas/Button.js
@@ -25,9 +25,9 @@ class Boton extends React.Component
             {
               return(
                 <tr>
-                  <td key={tarea.id}/*tareas={this.state.tareas.filter( (tarea) => tarea.class !== "pendiente" )}*/>{`nombre: ${tarea.name}`} <button onClick={() => this.handleClick(tarea.id)}>a</button></td>
-                  <td key={tarea.id}/*tareas={this.state.tareas.filter( (tarea) => tarea.class !== "pendiente" )}*/>{`nombre: ${tarea.name}`} <button onClick={() => this.handleClick(tarea.id)}>a</button></td>
-                  <td key={tarea.id}>{`nombre: ${tarea.name}`} <button onClick={() => this.handleClick(tarea.id)}>a</button></td>
+                  <td key={tarea.id}/*tareas={this.state.tareas.filter( (tarea) => tarea.class !== "pendiente" )}*/>{`nombre: ${tarea.name}`} <button onClick={() => this.handleClick(tarea.id)}>✓</button></td>
+                  <td key={tarea.id}/*tareas={this.state.tareas.filter( (tarea) => tarea.class !== "pendiente" )}*/>{`nombre: ${tarea.name}`} <button onClick={() => this.handleClick(tarea.id)}>✗</button></td>
+                  <td key={tarea.id}>{`nombre: ${tarea.name}`}</td>
                 </tr>
               )
             }
